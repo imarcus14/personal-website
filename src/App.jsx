f55f8bd4dec8e31a6740d1e36a8fac24683feb2f
imarcus14 <ivanmarcus1@gmail.com>
2024-12-19 16:24:51 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.scss'
+
+import reactLogo from './assets/react.svg'
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
