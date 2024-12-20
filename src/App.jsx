f3de326afee6d9fe036a12ec3917ca73099c056a
imarcus14 <ivanmarcus1@gmail.com>
2024-12-20 15:56:50 -0500

    Add background color for site and create header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,21 @@ import './App.scss'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import Header from './components/Header/Header'
+
 function App() {
  
 
   return (
     <>
+    <Header/>
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route> 
+      </Routes>
+    </BrowserRouter>
       
     </>
   )
