279ee092f49da2169d894ecb07ac1a63e601da7f
imarcus14 <ivanmarcus1@gmail.com>
2025-02-16 18:14:35 -0500

    CHange styling of page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
-import './App.scss'
+import "./App.scss";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Footer from './components/Footer/Footer'
-import Header from './components/Header/Header'
-import ProfilePage from './pages/ProfilePage/ProfilePage'
-import ProjectPage from './pages/ProjectPage/ProjectPage'
+// import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import ProjectPage from "./pages/ProjectPage/ProjectPage";
 
 function App() {
- 
-
   return (
     <>
-    
-    <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path="/" element={<ProfilePage/>}/>
-        <Route path="/projects" element={<ProjectPage/>}/>
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
-      
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProfilePage />} />
+          <Route path="/projects" element={<ProjectPage />} />
+        </Routes>
+        {/* <Footer /> */}
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,67 @@
-import "./Header.scss"
+import "./Header.scss";
 
 import { Link } from "react-router-dom";
+import github from "../../assets/icons/github.svg";
+import gmail from "../../assets/icons/gmail.svg";
+import ivan from "../../assets/icons/name.svg";
+import linkedin from "../../assets/icons/linkedin.png";
+import logo from "../../assets/icons/italic-i.svg";
 import { useState } from "react";
 
 const Header = () => {
+  const [activeLink, setActiveLink] = useState("/");
 
-    const [activeLink, setActiveLink] = useState("/");
+  const handleClick = (path) => {
+    setActiveLink(path);
+  };
 
-    const handleClick = (path) => {
-        setActiveLink(path);
-    };
+  return (
+    <div className="header">
+      <div className="header-logo">
+        <img className="i-logo" src={logo} alt="I logo" />
+        <img className="ivan-logo" src={ivan} alt="Ivan" />
+      </div>
 
-    return ( 
-        <div className="header">
-            <Link className={`header__link ${
-                    activeLink === "/" ? "header__link--active" : ""
-                }`}
-                to="/"
-                onClick={() => handleClick("/")}> 
-                <h3>Profile</h3>
-            </Link>
-            <Link className={`header__link ${
-                    activeLink === "/projects" ? "header__link--active" : ""
-                }`}
-                to="/projects"
-                onClick={() => handleClick("/projects")}> 
-                <h3>Projects</h3>
-            </Link>
-            
+      <div className="header-links">
+        <Link
+          className={`header__link ${
+            activeLink === "/" ? "header__link--active" : ""
+          }`}
+          to="/"
+          onClick={() => handleClick("/")}
+        >
+          <h3>Profile</h3>
+        </Link>
+        <Link
+          className={`header__link ${
+            activeLink === "/projects" ? "header__link--active" : ""
+          }`}
+          to="/projects"
+          onClick={() => handleClick("/projects")}
+        >
+          <h3>Projects</h3>
+        </Link>
+      </div>
+
+      <div className="contact-options">
+        <div className="contact-icon">
+          <Link to="https://www.linkedin.com/in/ivan-marcus/">
+            <img className="contact-img" src={linkedin} alt="linkedin icon" />
+          </Link>
         </div>
-     );
-}
- 
+        <div className="contact-icon">
+          <Link to="mailto:ivanmarcus1@gmail.com">
+            <img className="contact-img" src={gmail} alt="Gmail icon" />
+          </Link>
+        </div>
+        <div className="contact-icon">
+          <Link to="https://github.com/imarcus14">
+            <img className="contact-img" src={github} alt="Gmail icon" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Header;
